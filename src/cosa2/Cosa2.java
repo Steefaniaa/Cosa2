be1243395c1d334eaf3dc97057be198e737480e9
Administrador <Administrador@2DAM03>
2023-11-15 19:11:43 +0100

    cambio 2

--- src/cosa2/Cosa2.java
+++ src/cosa2/Cosa2.java
@@ -3,7 +3,9 @@ package cosa2;
 public class Cosa2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hola a todos");
+		
+		System.out.println("holita");
 
 	}
 
